692d2d9f7c96611498120d1d734956ec5ff284e6
asgersvenning <asgersvenning@gmail.com>
2023-11-09 11:10:38 +0100

    Changed the order of some functions in config

--- src/remote_data/config.py
+++ src/remote_data/config.py
@@ -1,66 +1,6 @@
 import os
 import yaml
 
-def get_config():
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    config_path = os.path.join(base_dir, 'pyremotedata_config.yaml')
-    
-    if not os.path.exists(config_path):
-        if input("Config file not found. Create default config file? (y/n): ").lower() == 'y':
-            create_default_config()
-        else:
-            raise FileNotFoundError("Config file not found at {}".format(config_path))
-
-    with open(config_path, 'r') as stream:
-        try:
-            config_data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            return None
-    
-    return config_data
-
-config = get_config()
-
-def get_this_config(this):
-    if this not in config:
-        raise ValueError("Key {} not found in config".format(this))
-    return config[this]
-
-def get_mount_config():
-    return config['mount']
-
-def get_dataloader_config():
-    return config['dataloader']
-
-def get_implicit_mount_config():
-    return config['implicit_mount']
-
-def deparse_args(config, what):
-    if not isinstance(what, str):
-        raise TypeError("Expected string, got {}".format(type(what)))
-    if what not in config:
-        raise ValueError("Key {} not found in config".format(what))
-    args = config[what]
-    if not isinstance(args, dict):
-        raise TypeError("Expected dict, got {}".format(type(args)))
-    
-    try:
-        arg_str = ""
-        for key, value in args.items():
-            if not isinstance(key, str):
-                raise TypeError("Expected string, got {}".format(type(key)))
-            if isinstance(value, bool):
-                arg_str += " --{}".format(key)
-            elif isinstance(value, str) or isinstance(value, int) or isinstance(value, float):
-                arg_str += " --{} {}".format(key, value)
-            else:
-                raise TypeError("Expected string, int, float or bool, got {}".format(type(value)))
-    except Exception as e:
-        raise yaml.YAMLError("Error while parsing args: {}".format(e))
-    
-    return arg_str
-    
 def create_default_config():
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     config_path = os.path.join(base_dir, 'pyremotedata_config.yaml')
@@ -122,3 +62,63 @@ def create_default_config():
     
     print("Created default config file at {}".format(config_path))
     print("Please edit the config file and fill in the required fields.")
+
+def get_config():
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    config_path = os.path.join(base_dir, 'pyremotedata_config.yaml')
+    
+    if not os.path.exists(config_path):
+        if input("Config file not found. Create default config file? (y/n): ").lower() == 'y':
+            create_default_config()
+        else:
+            raise FileNotFoundError("Config file not found at {}".format(config_path))
+
+    with open(config_path, 'r') as stream:
+        try:
+            config_data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
+    
+    return config_data
+
+config = get_config()
+
+def get_this_config(this):
+    if this not in config:
+        raise ValueError("Key {} not found in config".format(this))
+    return config[this]
+
+def get_mount_config():
+    return config['mount']
+
+def get_dataloader_config():
+    return config['dataloader']
+
+def get_implicit_mount_config():
+    return config['implicit_mount']
+
+def deparse_args(config, what):
+    if not isinstance(what, str):
+        raise TypeError("Expected string, got {}".format(type(what)))
+    if what not in config:
+        raise ValueError("Key {} not found in config".format(what))
+    args = config[what]
+    if not isinstance(args, dict):
+        raise TypeError("Expected dict, got {}".format(type(args)))
+    
+    try:
+        arg_str = ""
+        for key, value in args.items():
+            if not isinstance(key, str):
+                raise TypeError("Expected string, got {}".format(type(key)))
+            if isinstance(value, bool):
+                arg_str += " --{}".format(key)
+            elif isinstance(value, str) or isinstance(value, int) or isinstance(value, float):
+                arg_str += " --{} {}".format(key, value)
+            else:
+                raise TypeError("Expected string, int, float or bool, got {}".format(type(value)))
+    except Exception as e:
+        raise yaml.YAMLError("Error while parsing args: {}".format(e))
+    
+    return arg_str
