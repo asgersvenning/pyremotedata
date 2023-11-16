900b24286d99380c66a91aa02886c2a6fca03593
asgersvenning <asgersvenning@gmail.com>
2023-11-16 11:38:49 +0100

    Changing the upload-download bandwidth unit test to create a test file using openssl instead of downloading it from the internet, as well as printing the results.

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -3,7 +3,7 @@
 
 import wrapt_timeout_decorator
 from unittest.mock import patch
-import os
+import os, time
 
 @wrapt_timeout_decorator.timeout(10)
 def test_implicit_mount():
@@ -44,9 +44,14 @@ def test_upload_download():
         handler.start()
         print(handler.pwd())
         # Upload a test file to the mock SFTP server
-        upload_result = handler.put("https://link.testfile.org/15MB", "testfile.txt")
+        os.system("openssl rand -out localfile.txt -base64 $(( 2**27 * 9/16 ))")
+        start_upload = time.time()
+        upload_result = handler.put("localfile.txt", "testfile.txt")
+        end_upload = time.time()
         # Download the test file from the mock SFTP server
+        start_download = time.time()
         download_result = handler.download("testfile.txt", "testfile.txt")
+        end_download = time.time()
         # Get the local directory where the file should be downloaded to
         local_directory = handler.lpwd()
         # Sanity checks
@@ -57,10 +62,16 @@ def test_upload_download():
         if not (local_file_size > 10 and local_file_size < 20):
             raise RuntimeError("Something went wrong with the download. The file size is not correct.")
         # Cleanup
-        handler.stop()        
+        handler.stop() 
+        os.remove("localfile.txt")       
         from pyremotedata.config import remove_config
         remove_config()
-    print("Upload/download test passed.")
+
+    # Calculate results
+    upload_time = end_upload - start_upload
+    download_time = end_download - start_download
+    upload_speed, download_speed = 100/upload_time, 100/download_time
+    print(f'Upload/download test passed with upload {upload_speed:.1f} MB/s and download {download_speed:.1f} MB/s.') 
 
 try:
     test_implicit_mount()
