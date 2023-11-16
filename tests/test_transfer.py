7f5339fe43871595204de9f5257265477e29f568
asgersvenning <asgersvenning@gmail.com>
2023-11-16 12:11:03 +0100

    More improvements to the upload-download unit test

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -44,13 +44,19 @@ def test_upload_download():
         handler.start()
         print(handler.pwd())
         # Upload a test file to the mock SFTP server
-        os.system(f"openssl rand -out {handler.lpwd()}{os.sep}localfile.txt -base64 $(( 2**27 * 9/16 ))")
+        test_file_size = 10 # MB
+        n_rep = 10
+        generate_test_file_command = f"bash -c 'openssl rand -out {handler.lpwd()}{os.sep}localfile.txt -base64 {int(test_file_size * (10**6) * 3/4)}'"
+        print(f'Generating test file with command: {generate_test_file_command}')
+        os.system(generate_test_file_command)
         start_upload = time.time()
-        upload_result = handler.put("localfile.txt", "testfile.txt")
+        upload_result = handler.put("localfile.txt", "testfile.txt", execute=False)
+        upload_result = handler.execute_command(f'repeat -c {n_rep} -d 0.01 "rm -f testfile.txt && {upload_result}"')
         end_upload = time.time()
         # Download the test file from the mock SFTP server
         start_download = time.time()
-        download_result = handler.download("testfile.txt", "testfile.txt")
+        download_result = handler.download("testfile.txt", "testfile.txt", execute=False)
+        download_result = handler.execute_command(f'repeat -c {n_rep} -d 0.01 "(!rm -f testfile.txt) && {download_result}"')
         end_download = time.time()
         # Get the local directory where the file should be downloaded to
         local_directory = handler.lpwd()
@@ -59,17 +65,20 @@ def test_upload_download():
         local_file_size = os.path.getsize(os.path.join(local_directory, 'testfile.txt')) / 10**6
         if not local_file_exists:
             raise RuntimeError("Something went wrong with the download. The file does not exist locally.")
-        if not (local_file_size > 10 and local_file_size < 20):
-            raise RuntimeError("Something went wrong with the download. The file size is not correct.")
+        # Enforce a +/- 10% tolerance on the file size
+        if not (local_file_size > (0.9 * test_file_size) and local_file_size < (1.1 * test_file_size)):
+            raise RuntimeError(f"Something went wrong with the download. The file size (~{local_file_size:.2f} MB) is not correct.")
+        
         # Cleanup
+        os.remove(f"{handler.lpwd()}{os.sep}localfile.txt")
+        os.remove(f"{handler.lpwd()}{os.sep}testfile.txt")
         handler.stop() 
-        os.remove("localfile.txt")       
         from pyremotedata.config import remove_config
         remove_config()
 
     # Calculate results
-    upload_time = end_upload - start_upload
-    download_time = end_download - start_download
+    upload_time = (end_upload - start_upload) / n_rep - 0.01
+    download_time = (end_download - start_download) / n_rep - 0.01
     upload_speed, download_speed = 100/upload_time, 100/download_time
     print(f'Upload/download test passed with upload {upload_speed:.1f} MB/s and download {download_speed:.1f} MB/s.') 
 
