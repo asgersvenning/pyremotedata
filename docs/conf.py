e0909958e51743bbd915f55b87e17092a7ca25bf
asgersvenning <asgersvenning@gmail.com>
2024-06-07 10:44:23 +0200

    Changing sphinx theme to material

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'classic'
+html_theme = 'sphinx_material'
 html_css_files = ['custom.css']
 html_static_path = ['_static']
 html_baseurl = '_build/html/'
