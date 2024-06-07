428d1d0ea375d7b0b2f900a8220eba58281f6b1e
asgersvenning <asgersvenning@gmail.com>
2024-06-07 13:52:40 +0200

    Added repo to URL and a slightly more robust way of extracting the version number from the pyproject.toml in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../'))
 project = 'PyRemoteData'
 copyright = '2024, Asger Svenning'
 author = 'Asger Svenning'
-release = [re.search(r'version\s*=\s*\"(.*?)\"', line).group(1) for line in open('../pyproject.toml') if 'version' in line][0]
+release = [re.search(r'version\s*=\s*\"([^\"]*)\"', line).group(1) for line in open('../pyproject.toml') if 'version' in line][0]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -38,6 +38,31 @@ html_theme = 'sphinx_material'
 html_css_files = ['custom.css']
 html_static_path = ['_static']
 html_baseurl = '_build/html/'
+
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'PyRemoteData',
+
+    # # Specify a base_url used to generate sitemap.xml. If not
+    # # specified, then no sitemap will be built.
+    # 'base_url': 'https://project.github.io/project',
+
+    # # Set the color and the accent color
+    # 'color_primary': 'blue',
+    # 'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/asgersvenning/pyremotedata',
+    'repo_name': 'pyremotedata',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 3,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
 
 intersphinx_mapping = {
     'pyremotedata': ("https://asgersvenning.github.io/pyremotedata/" + html_baseurl, None),
