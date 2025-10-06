f20fa830898b8cf0c8ae6c9959c63be0123a91ae
asgersvenning <asgersvenning@gmail.com>
2025-10-06 16:37:16 +0200

    Update upload/download perf test

--- perf/upload_download.py
+++ perf/upload_download.py
@@ -3,7 +3,7 @@ import time
 import tempfile
 import os
 
-test_size = 10**6
+test_size = 3 * 10**6
     
 def make_mock_file(dir, size):
     with tempfile.NamedTemporaryFile(dir=dir, delete=False, suffix=".txt") as f:
@@ -12,7 +12,8 @@ def make_mock_file(dir, size):
 
 def perf_test(share_link_id : str | None):
     with tempfile.TemporaryDirectory() as upload_dir, tempfile.TemporaryDirectory() as download_dir:
-        mock_files = [make_mock_file(upload_dir, test_size) for _ in range(16)]
+        mock_files = [make_mock_file(upload_dir, test_size) for _ in range(32)]
+        total_data = test_size * len(mock_files)
 
         with IOHandler(user=share_link_id, password=share_link_id, verbose=True) as io:
             if not io.exists("testing", "directory"):
@@ -36,8 +37,8 @@ def perf_test(share_link_id : str | None):
         upload_time = end_upload - start_upload
         download_time = end_download - start_download
         delete_time = end_delete - start_delete
-
-        upload_speed, download_speed, delete_speed = test_size/upload_time, test_size/download_time, test_size/delete_time
+        
+        upload_speed, download_speed, delete_speed = total_data/upload_time, total_data/download_time, total_data/delete_time
 
         print("Upload speed: {} MB/s".format(upload_speed/10**6))
         print("Download speed: {} MB/s".format(download_speed/10**6))
